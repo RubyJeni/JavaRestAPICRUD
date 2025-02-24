1ee48bfb3b2a06a2f99d70b56e3cde6c2af52ce0
RubyJeni <59833584+RubyJeni@users.noreply.github.com>
2025-02-24 17:07:18 +0100

    test

---
+++ src/test/java/com/RJ/rest_demo/service/impl/CloudVendorServiceImplTests.java
@@ -0,0 +1,46 @@
+package com.RJ.rest_demo.service.impl;
+
+import com.RJ.rest_demo.model.CloudVendor;
+import com.RJ.rest_demo.repository.CloudVendorRepository;
+import com.RJ.rest_demo.service.CloudVendorService;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+class CloudVendorServiceImplTests {
+    @Mock
+    private CloudVendorRepository cloudVendorRepository;
+    private CloudVendorService cloudVendorService;
+    AutoCloseable autoCloseable;
+    CloudVendor cloudVendor;
+
+    @BeforeEach
+    void setUp() {
+        autoCloseable = MockitoAnnotations.openMocks(this);
+        cloudVendorService = new CloudVendorServiceImpl(cloudVendorRepository);
+        cloudVendor = new CloudVendor("1", "Amazon" , "USA", "xxxxx");
+    }
+
+    @AfterEach
+    void tearDown() throws Exception {
+        autoCloseable.close();
+    }
+
+    @Test
+    void TestCreateCloudVendor()
+    {
+        mock(CloudVendor.class);
+        mock(CloudVendorRepository.class);
+        when(cloudVendorRepository.save(cloudVendor)).thenReturn(cloudVendor);
+        assertThat(cloudVendorService.CreateCloudVendor(cloudVendor)).isEqualTo("Success");
+
+    }
+
+}
